ee0196b16788839057cfd3741032c3c6555f8433
Lorenzo05-creator <lory4jz@gmail.com>
2025-01-23 14:50:35 +0100

    Add files via upload

---
+++ esercizio-cicli2.js
@@ -0,0 +1,12 @@
+let num = 2;
+
+for (let i = 1; i <= 10; i++) {
+   console.log( num * i);
+ }
+
+
+    let num2 = 2;  
+    
+for (let i = 1; i <= 10; i++) {
+        let ndispari = ( (num2 * i) + 1);
+      }    
